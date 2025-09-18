c2f988ecc6bf322359c277149dd3dc2b4389fe52
bohi123 <bohi123@gmail.com>
2025-09-19 06:59:52 +0800

    Update route.ts

--- src/app/api/test/simple/route.ts
+++ src/app/api/test/simple/route.ts
@@ -1,1 +1,8 @@
+export const runtime = 'edge';
+
+export async function GET(request: Request, context: any) {
+  const result = await context.env.DB.prepare('SELECT * FROM users').all();
+  return new Response(JSON.stringify(result));
+}
+
 
